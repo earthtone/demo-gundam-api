d3b6472a118ba917f75d2de0b1df430ec99ddc7e
earthtone <earthlyillusion@gmail.com>
2021-12-03 23:16:31 +1100

    feat: add webscocket route

--- api/index.js
+++ api/index.js
@@ -1,12 +1,11 @@
 const hapi = require("@hapi/hapi")
 const { MongoClient, ObjectId } = require("mongodb")
-const url = process.env.MONGO_CONNECTION_STRING || "mongodb://localhost:27017"
-// const dbName = "gundam"
 
+const URL = process.env.MONGO_CONNECTION_STRING || "mongodb://localhost:27017"
 const PORT = process.env.PORT || 3000
 
 async function start () {
-  const client = await MongoClient.connect(url)
+  const client = await MongoClient.connect(URL)
   const db = client.db("gundam")
   const collection = db.collection("models")
 
@@ -15,6 +14,13 @@ async function start () {
     port: PORT
   })
 
+  await server.register({
+    plugin: require("hapi-pino")
+  })
+
+  await server.register(require('hapi-plugin-websocket'))
+
+  // GET every possible record
   server.route({
     method: "GET",
     path: "/",
@@ -24,15 +30,17 @@ async function start () {
     }
   })
 
+  // GET list of records
   server.route({
     method: "GET",
     path: "/models",
     async handler () {
-      const data = await collection.find({}, { _id: 1 }).map(x => x._id).toArray()
+      const data = await collection.find({}, { _id: 1 }).map(x => ({ id: x._id, name: x.name })).toArray()
       return { success: true, data }
     }
   })
 
+  // GET specific record by name
   server.route({
     method: "GET",
     path: "/model/{id}",
@@ -43,6 +51,8 @@ async function start () {
       else return { success: false, error: 'model not matched' }
     }
   })
+
+  // GET total count of records
   server.route({
     method: "GET",
     path: "/count",
@@ -52,8 +62,16 @@ async function start () {
     }
   })
 
-  await server.register({
-    plugin: require("hapi-pino")
+  // WS echo server
+  server.route({
+    method: "POST",
+    path: "/ws",
+    config: {
+      plugins: { websocket: { only: true, autoping: 30 * 1000 } }
+    },
+    handler (request) {
+      return { success: true, data: request.payload }
+    }
   })
 
   console.log(`starting server on ${PORT}`)
